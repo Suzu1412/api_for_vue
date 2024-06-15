68529cc4fe478bbf1569f637f0e694e46f7ce73f
Suzu <minato1932@gmail.com>
2024-06-15 11:23:43 -0400

    CORS Update Politics

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "http://127.0.0.1:5173"
+        # origins "*"
+        origins "http://localhost:5173/"
+                "http://127.0.0.1::5173/"
     
     resource "*",
         headers: :any,
